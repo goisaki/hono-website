95c4568bd5f1096c46d3a1cfe1bb1c82ad64666e
Kengo Watanabe <ken5@kwtc.jp>
2024-06-23 08:05:59 +0900

    feat: add `*.md` to routes

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,20 @@
 import { showRoutes } from 'hono/dev'
 import { createApp } from 'honox/server'
 
-const app = createApp()
+const app = createApp({
+  ROUTES: import.meta.glob(
+    [
+      '/app/routes/**/!(_*|*.test|*.spec).(ts|tsx|md|mdx)',
+      '/app/routes/.well-known/!(_*|*.test|*.spec).(ts|tsx|md|mdx)',
+      '/index.md',
+      '/docs/**/*.md',
+      '/examples/**/*.md'
+    ],
+    {
+      eager: true,
+    }
+  ),
+})
 
 showRoutes(app)
 
