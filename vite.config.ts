06a44c55c92023af094f1226b1b310bdc57aa3bb
Kengo Watanabe <ken5@kwtc.jp>
2024-06-23 08:09:36 +0900

    feat: render .md

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import pages from '@hono/vite-cloudflare-pages'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
+import mdx from '@mdx-js/rollup'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), pages()],
+  plugins: [
+    honox({ devServer: { adapter } }),
+    pages(),
+    mdx({
+      jsxImportSource: 'hono/jsx',
+    }),
+  ],
 })
